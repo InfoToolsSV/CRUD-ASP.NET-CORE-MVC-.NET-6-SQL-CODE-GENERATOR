03e5246636cc62a4a1052092c4c611893c08ac44
INFOTOOLSSV <infotoolssv2.0@gmail.com>
2023-07-08 20:35:43 -0600

    first commit

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUD_EF.Models;
+
+namespace CRUD_EF.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly CrudContext _context;
+
+        public PedidoController(CrudContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pedido
+        public async Task<IActionResult> Index()
+        {
+            var crudContext = _context.Pedidos.Include(p => p.Cliente);
+            return View(await crudContext.ToListAsync());
+        }
+
+        // GET: Pedido/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .FirstOrDefaultAsync(m => m.PedidoId == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedido/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClienteId", "Nombre");
+            return View();
+        }
+
+        // POST: Pedido/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("PedidoId,Descripcion,FechaPedido,ClienteId")] Pedido pedido
+        )
+        {
+            var cliente = await _context.Clientes.FindAsync(pedido.ClienteId);
+
+            if (cliente != null)
+            {
+                pedido.Cliente = cliente;
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(
+                _context.Clientes,
+                "ClienteId",
+                "Nombre",
+                pedido.ClienteId
+            );
+            return View(pedido);
+        }
+
+        // GET: Pedido/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(
+                _context.Clientes,
+                "ClienteId",
+                "Nombre",
+                pedido.ClienteId
+            );
+            return View(pedido);
+        }
+
+        // POST: Pedido/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind("PedidoId,Descripcion,FechaPedido,ClienteId")] Pedido pedido
+        )
+        {
+            if (id != pedido.PedidoId)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FindAsync(pedido.ClienteId);
+
+            if (cliente != null)
+            {
+                pedido.Cliente = cliente;
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.PedidoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(
+                _context.Clientes,
+                "ClienteId",
+                "Nombre",
+                pedido.ClienteId
+            );
+            return View(pedido);
+        }
+
+        // GET: Pedido/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .FirstOrDefaultAsync(m => m.PedidoId == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Pedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido != null)
+                _context.Pedidos.Remove(pedido);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedidos.Any(e => e.PedidoId == id);
+        }
+    }
+}
---
+++ Models/CrudContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_EF.Models;
+
+public partial class CrudContext : DbContext
+{
+    public CrudContext(DbContextOptions<CrudContext> options)
+        : base(options)
+    {
+        Clientes = Set<Cliente>();
+        Pedidos = Set<Pedido>();
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Pedido> Pedidos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.ClienteId).HasName("PK__Cliente__71ABD087FE2705FC");
+
+            entity.ToTable("Cliente");
+
+            entity.Property(e => e.Email).HasMaxLength(100).IsUnicode(false);
+            entity.Property(e => e.Nombre).HasMaxLength(50).IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Pedido>(entity =>
+        {
+            entity.HasKey(e => e.PedidoId).HasName("PK__Pedido__09BA14304E05834D");
+
+            entity.ToTable("Pedido");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(100).IsUnicode(false);
+            entity.Property(e => e.FechaPedido).HasColumnType("datetime");
+
+            entity
+                .HasOne(d => d.Cliente)
+                .WithMany(p => p.Pedidos)
+                .HasForeignKey(d => d.ClienteId)
+                .HasConstraintName("FK_Cliente_Pedido_ClienteId");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
